0ee3e430e5ec22ad05bf8813c260c392b6201938
Simon <simon@aam-digital.com>
2025-01-14 20:10:56 +0100

    Initial Commit

---
+++ PostureClient/PostureClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostureClientApp.swift
+//  PostureClient
+//
+//  Created by Simon Hanselmann on 14.01.25.
+//
+
+import SwiftUI
+
+@main
+struct PostureClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
